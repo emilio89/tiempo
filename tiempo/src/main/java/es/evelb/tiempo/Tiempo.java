07c0158fe3bf10197c728ad4fa3ae012b9059e70
Emilio <Emilio@Emilio>
2018-11-15 19:38:27 +0100

    inicial

---
+++ tiempo/src/main/java/es/evelb/tiempo/Tiempo.java
@@ -0,0 +1,12 @@
+package es.evelb.tiempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tiempo {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Tiempo.class, args);
+    }
+
+}
